e91ed1068d2c0c65ad016262a649302b3470cf2e
rlagksruf16 <rlagksruf16@gmail.com>
2020-11-19 22:33:33 +0900

    solve 11066 from BOJ / DP

---
+++ BOJ/11066.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+ 
+using namespace std;
+
+class Car{
+    public:
+    int x,y,cost, dir; // x, y, 요금, 방향
+};
+ 
+int solution(vector<vector<int>> board) {
+    int answer = 999999999, mx[] = {0,1,0,-1}, my[] = {1,0,-1,0}, N = board.size();
+    queue<Car> q;    
+    Car c;
+    c.x = 0, c.y = 0, c.cost = 0, c.dir = 10;
+    q.push(c);
+    board[0][0] = 1;
+    
+    while(!q.empty())
+    {
+        Car a = q.front();
+        q.pop();
+
+				// 3번 조건
+        if(a.x == N - 1 && a.y == N - 1)
+        {
+            if(answer > a.cost) answer = a.cost;
+            continue;
+        }
+        for(int i=0;i<4;i++)
+        {
+            int nx = a.x + mx[i];
+            int ny = a.y + my[i];
+            
+            if( nx<0 || ny<0 || nx>=N || ny>=N || board[nx][ny] == 1) continue;
+            
+            int nc = 0;
+
+            // 1번 조건
+            if(a.dir == 10 || a.dir == i)
+                nc = a.cost + 100;
+            else if(a.dir != i)
+                nc = a.cost + 600;
+            
+						// 2번 조건
+            if(board[nx][ny] == 0 || board[nx][ny] >= nc)
+            {
+                board[nx][ny] = nc;
+                Car p;
+                p.x = nx, p.y = ny, p.cost = nc, p.dir = i;
+                q.push(p);
+            }
+        }
+        
+    }
+    return answer;
+}
