fa21178bc2d6cb12431e08093b5f3a2312f8d129
rlagksruf16 <rlagksruf16@gmail.com>
2021-02-23 23:50:01 +0900

    solve 4195 from BOJ / union-find, gold

---
+++ BOJ/Union_Find/4195.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int N, M;
+string f1, f2;
+
+int parent[200002];
+int nod[200002];
+
+int find(int z) {
+    if(parent[z] == z)
+        return z;
+
+    return parent[z] = find(parent[z]);
+}
+
+int merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+    
+    if(x != y) {
+        parent[y] = x;
+        nod[x] += nod[y];
+        nod[y] = 1; // 1로 초기화
+    }
+    return nod[x];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+    cin >> N;
+    
+    for(int i = 0; i < N; i++) {
+        cin >> M;
+        
+        map<string,int> f;
+        int num = 1;
+        
+        for(int j = 1; j <= 2*M; j++) {
+            parent[j] = j;
+            nod[j] = 1;
+        }
+        for(int z = 0; z < M; z++) {
+            cin >> f1 >> f2;
+            if(f.count(f1) == 0)
+                f[f1] = num++;
+            if(f.count(f2) == 0)
+                f[f2] = num++;
+            
+            cout << merge(f[f1],f[f2]);
+            cout << "\n";
+        }
+        
+        
+    }
+    return 0;
+}
